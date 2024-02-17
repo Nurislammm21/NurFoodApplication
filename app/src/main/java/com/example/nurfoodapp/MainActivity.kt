98d73efdc6cbf6c514e15d05813e77cf9d2f78bf
Nurislam <gggrrraaa1235@gmail.com>
2024-02-18 01:48:57 +0600

    7th commit  connecting menu navigation in MainActivity

--- app/src/main/java/com/example/nurfoodapp/MainActivity.kt
+++ app/src/main/java/com/example/nurfoodapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.nurfoodapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.nurfoodapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = Navigation.findNavController(this,R.id.host_fragment)
+        NavigationUI.setupWithNavController(binding.btnNav,navController)
     }
 }
