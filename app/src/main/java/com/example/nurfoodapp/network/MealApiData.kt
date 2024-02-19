e671d2ebe0628c9229f1a56c54ea24caafd3753a
Nurislam <gggrrraaa1235@gmail.com>
2024-02-19 17:54:38 +0600

    12th commit  created instance object of the url for build our api

--- app/src/main/java/com/example/nurfoodapp/network/MealApiData.kt
+++ app/src/main/java/com/example/nurfoodapp/network/MealApiData.kt
@@ -1,4 +1,14 @@
 package com.example.nurfoodapp.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object MealApiData {
+    private const val BASE_URL = "https://www.themealdb.com/api/json/v1/1/"
+
+    val mealApi: MealApi by lazy {
+        Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+            .build().create(MealApi::class.java)
+    }
+
 }
