240f37524a295e15e75bdb28f9b3455283a23b68
Nurislam <gggrrraaa1235@gmail.com>
2024-02-19 18:45:15 +0600

    13th commit  created request response to glide take img from request api in home fragment

--- app/src/main/java/com/example/nurfoodapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/nurfoodapp/fragments/HomeFragment.kt
@@ -1,25 +1,56 @@
 package com.example.nurfoodapp.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.nurfoodapp.R
+import com.example.nurfoodapp.data.Meal
+import com.example.nurfoodapp.data.MealList
+import com.example.nurfoodapp.databinding.FragmentHomeBinding
+import com.example.nurfoodapp.network.MealApiData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class HomeFragment : Fragment() {
-
+private lateinit var binding : FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View{
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initRequest()
+    }
+
+
+    private fun initRequest() {
+        MealApiData.mealApi.getRandomMeal().enqueue(object : Callback<MealList> {
+            override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
+                if (response.body() != null) {
+                    val randomMeal: Meal = response.body()!!.meals[0]
+                    Glide.with(this@HomeFragment).load(randomMeal.strMealThumb)
+                        .into(binding.imageRandomMeal)
+                } else {
+                    return
+                }
+            }
+
+            override fun onFailure(call: Call<MealList>, t: Throwable) {
+                Log.e("HomeFragment", t.message.toString())
+            }
+
+        })
+
     }
 }
