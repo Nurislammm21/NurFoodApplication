d85cd02b940d040db4cde9aad2e493cdd604c822
Nurislam <gggrrraaa1235@gmail.com>
2024-02-19 17:19:13 +0600

    10th commit  added data classes from json take it all classes for packages

--- app/src/main/java/com/example/nurfoodapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/nurfoodapp/activities/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.example.nurfoodapp
+package com.example.nurfoodapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import com.example.nurfoodapp.R
 import com.example.nurfoodapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navController = Navigation.findNavController(this,R.id.host_fragment)
+        val navController = Navigation.findNavController(this, R.id.host_fragment)
         NavigationUI.setupWithNavController(binding.btnNav,navController)
     }
 }
---
+++ app/src/main/java/com/example/nurfoodapp/data/MealList.kt
@@ -0,0 +1,5 @@
+package com.example.nurfoodapp.data
+
+data class MealList(
+    val meals: List<Meal>
+)
