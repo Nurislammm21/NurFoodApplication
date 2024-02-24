a93491d216e82ab8b32d2bf0934846c1baa6c851
Nurislam <gggrrraaa1235@gmail.com>
2024-02-24 23:07:34 +0600

    17th commit created adapter for PopularMeals in rcVIew

---
+++ app/src/main/java/com/example/nurfoodapp/adapters/MostPopularAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.nurfoodapp.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.nurfoodapp.data.CategoryMeals
+import com.example.nurfoodapp.databinding.PopularItemsBinding
+
+class MostPopularAdapter() : RecyclerView.Adapter<MostPopularAdapter.PopularMealViewHolder>() {
+private var mealsList = ArrayList<CategoryMeals>()
+
+    fun setMeals(mealsList: ArrayList<CategoryMeals>){
+        this.mealsList = mealsList
+        notifyDataSetChanged()
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularMealViewHolder {
+        return PopularMealViewHolder(PopularItemsBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun getItemCount(): Int = mealsList.size
+
+    override fun onBindViewHolder(holder: PopularMealViewHolder, position: Int) {
+        Glide.with(holder.itemView).load(mealsList[position].strMealThumb).into(holder.binding.imagePopularMealItem)
+    }
+
+    class PopularMealViewHolder( val binding: PopularItemsBinding) : RecyclerView.ViewHolder(binding.root){
+
+    }
+}
---
+++ app/src/main/java/com/example/nurfoodapp/data/CategoryList.kt
@@ -0,0 +1,5 @@
+package com.example.nurfoodapp.data
+
+data class CategoryList(
+    val meals: List<CategoryMeals>
+)
